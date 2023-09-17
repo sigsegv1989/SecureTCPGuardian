720ac4cdca8d3f92c7110bcc48910557045d0971
Abhijeet Rokade <sigsegv1989@gmail.com>
2023-09-17 17:15:56 +0000

    Enhance handleRequest functionality with Connection Timeout Handling
    - Implement a timeout mechanism for reading messages from clients.
    - Distinguish between timeout errors and other errors during reading.
    - Added context handling for better control over connection timeouts.
    
    This commit enhances the `handleRequest` function with Connection Timeout Handling,
    which prevents connections from hanging indefinitely and enforces a timeout mechanism.
    It allows the server to handle connection timeouts more gracefully and efficiently,
    improving resource management and overall robustness.

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"context"
 	"net"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -48,6 +50,7 @@ func (s *TCPServer) Start() {
 			s.logger.Errorf("Error accepting connection: %v", err)
 			continue
 		}
+
 		go s.handleRequest(conn)
 	}
 }
@@ -63,11 +66,57 @@ func (s *TCPServer) handleRequest(conn net.Conn) {
 	// Handle the incoming request here
 	s.logger.Infof("Accepted connection from %s", conn.RemoteAddr())
 
-	// You can implement your request handling logic here.
-	// For simplicity, we just send a "Hello, World!" response.
-	response := "HTTP/1.1 200 OK \r\n\r\nHello, World!\r\n"
-	_, err := conn.Write([]byte(response))
-	if err != nil {
-		s.logger.Errorf("Error writing response: %v", err)
-	}
+	// Create a context with a 120-second timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
+	defer cancel()
+
+	// Create a channel to signal when the operation is complete
+	done := make(chan struct{})
+
+	func() {
+		defer close(done)
+
+		// Create a buffer for reading messages
+		buf := make([]byte, 1024)
+
+		for {
+			select {
+			case <-ctx.Done():
+				err := ctx.Err()
+				if err == context.DeadlineExceeded {
+					s.logger.Infof("Connection from %s timed out", conn.RemoteAddr())
+				} else {
+					s.logger.Errorf("Context error: %v", err)
+				}
+				return
+			default:
+				// Set a read deadline for each message read
+				conn.SetReadDeadline(time.Now().Add(120 * time.Second))
+
+				n, err := conn.Read(buf)
+				if err != nil {
+					if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+						s.logger.Infof("Read timeout from %s", conn.RemoteAddr())
+					} else {
+						s.logger.Errorf("Error reading from client: %v", err)
+					}
+					return
+				}
+
+				clientMessage := string(buf[:(n - 1)])
+				s.logger.Infof("Received message from client: %s", clientMessage)
+
+				//response := "HTTP/1.1 200 OK \r\n\r\n" + clientMessage + "!\r\n"
+				_, err = conn.Write([]byte(clientMessage + "!\n"))
+				if err != nil {
+					s.logger.Errorf("Error writing response: %v", err)
+				}
+
+				// reset the read deadline for the next message
+				conn.SetReadDeadline(time.Time{})
+			}
+		}
+	}()
+
+	<-done
 }
